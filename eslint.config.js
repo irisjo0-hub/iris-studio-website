c8f4cac925170d243fc1d3d44a00cdb2d643f3b6
irisjo0-hub <iris.contact.jo@gmail.com>
2026-09-06 06:31:42 +0300

    Lint JavaScript and JSX sources

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,22 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
